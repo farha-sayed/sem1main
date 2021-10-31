8707d0fc74920fd9ec2f82842339ae1d7f5f2546
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2021-10-31 02:41:41 +0000

    Add files via upload

---
+++ temperaturedraft.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+double f;
+int celsius;
+printf("Enter temperature in degrees celsius");
+scanf("%i", &celsius)
+f = (9/5)*(float)celsius + 32;
+printf("The temp in fahrenheit is &lf", f);
+}
