47fd61af80b4a5f516fe28374b720de5650fd7ef
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2021-11-06 21:35:28 +0000

    Update gravitationalforce.c

--- gravitationalforce.c
+++ gravitationalforce.c
@@ -5,12 +5,12 @@ int main()
 float F, G, m1, m2, r;
 G = 6.674* pow(10,-11);
 printf("Please enter the mass of both objects one after the other");
-scanf("%lf %lf", &m1, &m2);
+scanf("%e %e", &m1, &m2);
 printf("Please enter the distance between them");
-scanf("%lf", &r);
+scanf("%e", &r);
 
 //calculating
-F = G*((m1*m2)/(pow(r,2)));
+F = (G*m1*m2)/(pow(r,2));
 
 printf("The gravitational force between these two points is %e", F);
 
