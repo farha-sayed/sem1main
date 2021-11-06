45dcd98949674d913bda74f6817701b0601001b0
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2021-11-06 21:35:53 +0000

    Update distancebtwtwopoints.c

--- distancebtwtwopoints.c
+++ distancebtwtwopoints.c
@@ -11,6 +11,6 @@ scanf("%lf %lf", &x1, &y1);
 printf("Please enter the coordinates for first point(x1,y1)\n");
 scanf("%lf %lf", &x2, &y2);
 
-d = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
-
+d = sqrt(pow((x2-x1),2) + pow((y2-y1),2);
+printf("%lf", d);
 }
