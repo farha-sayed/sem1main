e4049d671f5287b4f5f1e5f6e548a95a15513b53
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2021-11-06 21:35:03 +0000

    Update gbp_to_yen.c

--- gbp_to_yen.c
+++ gbp_to_yen.c
@@ -7,7 +7,11 @@ double p, y;
 printf("Please enter amt in pounds");
 scanf("%lf", &p);
 y = 153.09*p;
-printf("This amt in Japanese yen is %lf", y);
+printf("This amt in Japanese yen is \n %lf \n", y);
+if(y>1000000)
+{
+  printf("Wow you are a millionaire in Japan!!");
+}
 return 0;
 
 }
