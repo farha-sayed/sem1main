2af7cf8e4dfb9f1c295a77a180fe7c88621022f0
farha-sayed <91874664+farha-sayed@users.noreply.github.com>
2021-11-06 21:32:26 +0000

    Create gbp_to_yen.c

---
+++ gbp_to_yen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+double p, y;
+printf("Please enter amt in pounds");
+scanf("%lf", &p);
+y = 153.09*p;
+printf("This amt in Japanese yen is %lf", y);
+return 0;
+
+}
